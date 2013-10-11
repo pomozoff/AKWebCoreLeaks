b0ca8906fdbf196b15d842695db30f5fb16034a8
Anton Pomozov <pomozov@shtrih-m.ru>
2013-10-11 13:08:39 +0400

    Initial Commit

---
+++ AKWebCoreLeaks/AKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AKAppDelegate.h
+//  AKWebCoreLeaks
+//
+//  Created by Anton Pomozov on 11.10.13.
+//  Copyright (c) 2013 Akademon Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AKWebCoreLeaks/AKViewController.h
@@ -0,0 +1,13 @@
+//
+//  AKViewController.h
+//  AKWebCoreLeaks
+//
+//  Created by Anton Pomozov on 11.10.13.
+//  Copyright (c) 2013 Akademon Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AKViewController : UIViewController
+
+@end
